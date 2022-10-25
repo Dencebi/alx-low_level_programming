44cfa9893461cb4ed9e7a94d16de3326aa512656
Dencebi <dencefavour@gmail.com>
2022-10-25 00:31:51 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,7 +3,7 @@
 
 /**
  * print_array - Prints an inputted number of elements
- * of an array integers.
+ * of an array of integers.
  * @a: The array of integers.
  * @n: The number of elements to be printed.
  */
@@ -15,9 +15,11 @@ void print_array(int *a, int n)
 	{
 		printf("%d", a[index]);
 
-
 		if (index == n - 1)
 			continue;
-	printf(", ");
+
+		printf(", ");
 	}
+
+	printf("\n");
 }
